43c823fc561e7fb699a3fb8e47b6d4197d879654
Yannik <dieschlafmuetze@gmail.com>
2024-12-05 23:42:09 +0100

    release 1.0

--- Hitting Set Verifier/src/test/java/de/uni/bremen/hs/VerifierTest.java
+++ Hitting Set Verifier/src/test/java/de/uni/bremen/hs/VerifierTest.java
@@ -1,5 +1,7 @@
 package de.uni.bremen.hs;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import de.uni.bremen.hs.io.Importer;
 import de.uni.bremen.hs.model.HyperGraph;
 import de.uni.bremen.hs.model.Solution;
@@ -8,32 +10,33 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class VerifierTest {
 
-    final static String TEST_DIR = "testset";
+  final static String TEST_DIR = "testset";
 
-    @Test
-    void testAll() {
-        Stream.of(Objects.requireNonNull(new File(TEST_DIR).listFiles()))
-                .filter(file -> !file.isDirectory())
-                .forEach(file -> {
-                    if(file.getName().endsWith(".hgr")) {
-                        try {
-                            Importer importer = new Importer();
-                            HyperGraph g = importer.readHyperGraph(file.getPath());
-                            Solution sol = importer.readSolution(file.getPath().replace(".hgr", ".sol"));
+  @Test
+  void testAll() throws URISyntaxException {
+    var testDir = new File(Objects.requireNonNull(getClass().getClassLoader().getResource(TEST_DIR)).toURI());
 
-                            HSVerifier verifier = new HSVerifier();
-                            assertTrue(verifier.verify(g, sol));
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-    }
+    Stream.of(Objects.requireNonNull(testDir.listFiles()))
+        .filter(file -> !file.isDirectory())
+        .forEach(file -> {
+          if (file.getName().endsWith(".hgr")) {
+            try {
+              Importer importer = new Importer();
+              HyperGraph g = importer.readHyperGraph(file.getPath());
+              Solution sol = importer.readSolution(file.getPath().replace(".hgr", ".sol"));
+
+              HSVerifier verifier = new HSVerifier();
+              assertTrue(verifier.verify(g, sol));
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          }
+        });
+  }
 }
